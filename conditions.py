67b712ee1fc00a8d4b4be5b3e3c7cff6e0356d46
Binish John <binish.heptas@gmail.com>
2015-08-12 03:00:17 -0700

    Basics

---
+++ conditions.py
@@ -0,0 +1,27 @@
+
+
+def if_condition():
+  a = raw_input("Enter A Number :")
+  if int(a)%2 == 0 :
+     print "Even Number"
+
+def if_else_condition():
+  a = raw_input("Enter A Number :")
+  if int(a)%2 == 0 :
+     print "Even Number"
+  else :
+     print "Odd Number"
+
+
+def if_else_ladder():
+  a = raw_input("Enter A Number :")
+  if int(a) > 0 :
+     print "Positive Number"
+  elif int(a) < 0 :
+     print "Negative Number"
+  else: 
+     print "Zero"
+
+  
+
+
---
+++ functions.py
@@ -0,0 +1,21 @@
+
+
+def function_1():
+   print "Function One"
+
+
+def function_2(name):
+   print "Hello " , name
+
+def function_3(name,age):
+   print "Hello " , name , age
+
+def function_4(name):
+   return name
+
+
+def function_5(name, age=26):
+   print "Hello " , name , age
+
+def function_6(name,age):
+   return name , age
